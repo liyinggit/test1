cb38c8873c7e46f8aa84a625f33a82a5a9340c97
liyinggit <2501830002@qq.com>
2019-02-15 10:27:15 +0800

    helloworld

---
+++ src/component/login.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class Login extends Component{
+
+    render(){
+        return(
+          <div className="App">
+              <a>helloworld!</a>
+          </div>
+        );
+    }
+}
+
+export default Login;
---
+++ src/navigation/rootnavigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route} from 'react-router-dom';
+import App from "../App";
+
+
+const Container = (props) => {
+    return (
+        <div>{props.children}</div>
+    );
+};
+
+const routes = (
+    <Route path="/" component={Container} >
+        <Route path="/app" component={App} />
+    </Route>
+);
+
+export default routes;
